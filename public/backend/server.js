e60874e10fa0d380881c24ed616f3ebd546aac6a
SreeTarak2 <122889552+SreeTarak2@users.noreply.github.com>
2025-04-14 15:45:57 +0530

    Update server.js

--- public/backend/server.js
+++ public/backend/server.js
@@ -4,9 +4,7 @@ const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors({
-  origin: "https://sreetarak2.github.io"
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
